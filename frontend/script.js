d60b6e701cd34836bd0fd9d845a2ede4506deca9
sohelazure <sohel.shaik@sailssoftware.com>
2025-11-11 12:50:29 +0530

    creating datatable using pipeline

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,7 @@ if (window.location.hostname === "localhost") {
 } else {
   // Running in production (frontend served from Kubernetes)
   // Frontend container talks to backend-service inside cluster
-  apiBaseUrl = "http://34.100.187.174/tasks";
+  apiBaseUrl = "http://34.93.76.225/tasks";
 }
 
 async function fetchTasks() {
