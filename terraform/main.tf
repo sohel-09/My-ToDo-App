d4e89d08a0ef92194196f5e3d45bbc41b5a5a787
sohelazure <sohel.shaik@sailssoftware.com>
2025-11-17 22:00:14 +0530

    changes in CI/CD

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,52 +10,41 @@ terraform {
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-  credentials = base64decode(var.gcp_credentials)
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+  # credentials = base64decode(var.gcp_credentials)    # CD Pipeline injects this
 }
 
-# GKE Cluster
-resource "google_container_cluster" "todo_cluster" {
+# -------------------------------
+# READ EXISTING GKE CLUSTER
+# -------------------------------
+data "google_container_cluster" "todo_cluster" {
   name     = "todo-cluster"
   location = var.zone
-
-  remove_default_node_pool = true
-  initial_node_count = 1
 }
 
-# Node pool
+# -------------------------------
+# CREATE ONLY NODE POOL (optional)
+# -------------------------------
 resource "google_container_node_pool" "todo_nodes" {
   name       = "todo-nodes"
-  cluster    = google_container_cluster.todo_cluster.name
+  cluster    = data.google_container_cluster.todo_cluster.id
   location   = var.zone
-  node_count = 2
 
   node_config {
     machine_type = "e2-medium"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
   }
+
+  initial_node_count = 1
 }
 
-# Artifact Registry
-resource "google_artifact_registry_repository" "todo_repo" {
-  location      = var.region
+# -------------------------------
+# READ EXISTING ARTIFACT REGISTRY REPO
+# -------------------------------
+data "google_artifact_registry_repository" "todo_repo" {
   repository_id = "todo-repo"
-  format        = "DOCKER"
-  description   = "Repository for ToDo App images"
+  location      = var.region
 }
 
-# Static IP for backend
-resource "google_compute_address" "backend_static_ip" {
-  name   = "backend-static-ip"
-  region = var.region
-}
 
-# Static IP for frontend
-resource "google_compute_address" "frontend_static_ip" {
-  name   = "frontend-static-ip"
-  region = var.region
-}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,10 @@
 variable "project_id" {}
 variable "region" { default = "asia-south1" }
 variable "zone" { default = "asia-south1-a" }
+
 variable "gcp_credentials" {
-  type        = string
-  description = "GCP SA key in JSON format"
+  type = string
+  description = "Base64 encoded service account key"
 }
+
 
