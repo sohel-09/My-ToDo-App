46d969f22928a47e2556fbc7ab2d60e60735d2b4
sohelazure <sohel.shaik@sailssoftware.com>
2025-11-20 12:11:06 +0530

    complete working project from start to end

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,3 @@ output "artifact_repo_name" {
   value = google_artifact_registry_repository.todo_repo.repository_id
 }
 
-# output "backend_static_ip" {
-#   value = google_compute_address.backend_ip.address
-# }
